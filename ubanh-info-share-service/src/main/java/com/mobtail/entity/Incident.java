6952d9629fb36d4d802f95dbc1582449604acb01
Eric Nascimento <ericsantosnascimento@gmail.com>
2018-10-22 00:31:24 +0200

    feat: Adding database configuration

--- ubanh-info-share-service/src/main/java/com/mobtail/entity/Incident.java
+++ ubanh-info-share-service/src/main/java/com/mobtail/entity/Incident.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Incident {
 
     @Id
